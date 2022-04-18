20376cb7a8ab998c80609dd70f35770d97c74e47
Victoria-Fire <vik.fire@yandex.ru>
2022-04-18 18:21:04 +0300

    per_class_hierarchy

--- src/main/java/by/academy/it/reunova/entity/Person.java
+++ src/main/java/by/academy/it/reunova/entity/Person.java
@@ -1,7 +1,21 @@
 package by.academy.it.reunova.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
 public class Person {
 
+    @Column(name = "watcher_name")
     private String name;
+
+    @Column(name = "watcher_surname")
     private String surname;
 }
--- src/main/java/by/academy/it/reunova/entity/Task.java
+++ src/main/java/by/academy/it/reunova/entity/Task.java
@@ -1,8 +1,30 @@
 package by.academy.it.reunova.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TASK_TYPE",
+        discriminatorType = DiscriminatorType.CHAR)
+@DiscriminatorValue("T")
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Integer id;
-    private String name;
+
+    @Column
+    private String nameTask;
+
+    @Column
     private String description;
 }
